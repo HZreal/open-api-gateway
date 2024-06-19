3aff2b7ff42a928af26cf0610f04a0d0dcaf627e
HZreal <hzreal0823@gmail.com>
2024-06-20 01:00:06 +0800

    init

---
+++ src/main/java/com/huang/openplatformgateway/OpenPlatformGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.huang.openplatformgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenPlatformGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenPlatformGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huang/openplatformgateway/OpenPlatformGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huang.openplatformgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenPlatformGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
